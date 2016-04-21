d8ae987b300d60ce644bf0b929b2559e6f6fe5d5
Dell <Dell@Dell-PC>
2016-04-21 23:49:57 +0100

    third commit.

--- src/Hello.java
+++ src/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 		      System.out.println("Hello, World");
 		      System.out.println("123");
 		      System.out.println("pqr");
+		      System.out.println("abc");
 		   }
 		}
 
